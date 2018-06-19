70348457f825024c037515bab20aa80e332a5715
bryankerr1986 <bryankerr1986@gmail.com>
2018-06-18 20:56:57 -0700

    Changed 'conda install pywinauto' to 'pip install pywinauto'

--- setup.py
+++ setup.py
@@ -1,8 +1,31 @@
+################################################################################
+##  TITLE: Setup Python Environment
+##
+##  FILENAME: setup.py
+##
+##  AUTHOR: Bryan Kerr
+##
+##  REVISION: 1.2
+##  DATE: 06/18/2018
+##
+##  DESCRIPTION:
+##  
+##  Makes sure all required packages and drivers are installed for the Max1000
+##  mut project.
+##  
+################################################################################
+##  REVISION HISTORY (MANUAL):
+##  06/01/2018 BEK - Initial coding
+##  06/18/2018 BEK - Changed "conda install pywinauto" to "pip install pywinauto"
+##
+################################################################################
+## Library declarations
+################################################################################
 import subprocess
 
 subprocess.call("conda install numpy",      shell=True)
 subprocess.call("conda install matplotlib", shell=True)
-subprocess.call("conda install pywinauto",  shell=True)
+subprocess.call("pip install pywinauto",    shell=True)
 subprocess.call("conda install pyserial",   shell=True)
 
 from pywinauto.application import Application
